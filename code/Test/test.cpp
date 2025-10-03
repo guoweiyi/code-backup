3327fc39f27caed19b75c5953682025a35372284
Guoweiyi <gwy@gwy.fun>
2025-10-03 10:05:37 +0800

    ENV

---
+++ code/Test/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    scanf("%d",&a); 
+    scanf("%d",&b); 
+    a+=1;
+    a+=b;
+    a+=2;
+    printf("%d\n",a);
+    return 0;
+}
